f3a3fb8c07db5e8d658a7d04e9727e5d1f86d053
mxdev9917 <eh.dev9917@gmail.com>
2026-04-24 00:53:01 +0700

    feat: implement ApiConfig class and update transitive dependencies

--- lib/api_config.dart
+++ lib/api_config.dart
@@ -5,4 +5,7 @@ class ApiConfig {
   static const String ratesUrl = '$baseUrl/api/exchange-rates';
   static const String ordersUrl = '$baseUrl/api/orders';
   static const String shopsUrl = '$baseUrl/api/shops';
+
+
+  
 }
